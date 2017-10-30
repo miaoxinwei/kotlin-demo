bdca6043e1f90eebdc9d38e6883685dd46b5730b
lico <254975828@qq.com>
2017-10-30 17:26:21 +0800

    init project

---
+++ kotlin/src/main/kotlin/com/lico/study/kotlin/kotlin/KotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.lico.study.kotlin.kotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinApplication::class.java, *args)
+}
